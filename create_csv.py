12743250643da88c44ed29bbdf9480fd7f259376
1lello <flowmushi@gmail.com>
2023-08-17 22:20:59 +0300

    added

--- create_csv.py
+++ create_csv.py
@@ -9,39 +9,30 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         
-        # Read the filename from the 'filename' tag
-        filename = root.find('filename').text
+        print(f"Processing file: {xml_file}")  # Print the current XML file being processed
         
-        # Read the image width and height from the 'size' tag
+        filename = root.find('filename').text
+        print(f"  Filename: {filename}")  # Print the filename
+        
         width = int(root.find('size').find('width').text)
         height = int(root.find('size').find('height').text)
+        print(f"  Width: {width}, Height: {height}")  # Print the width and height
         
-        # Iterate over each 'object' tag in the XML
         for member in root.findall('object'):
-            # Read the class name from the 'name' tag within the 'object' tag
             class_name = member.find('name').text
+            print(f"  Class Name: {class_name}")  # Print the class name
             
-            # Read the bounding box coordinates from the 'bndbox' tag within the 'object' tag
             xmin = int(member.find('bndbox').find('xmin').text)
             ymin = int(member.find('bndbox').find('ymin').text)
             xmax = int(member.find('bndbox').find('xmax').text)
             ymax = int(member.find('bndbox').find('ymax').text)
+            print(f"  Bounding Box: {xmin}, {ymin}, {xmax}, {ymax}")  # Print the bounding box
             
-            # Append the extracted information as a tuple to the xml_list
             xml_list.append((filename, width, height, class_name, xmin, ymin, xmax, ymax))
-    
-    # Create a DataFrame from the xml_list
+            
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     
     return xml_df
 
-def main():
-    for folder in ['train','validation']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
-        xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
-        print('Successfully converted xml to csv.')
-
-main()
 
