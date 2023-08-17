860863198a5cce0333c5e2d62a9634a5b624e145
1lello <flowmushi@gmail.com>
2023-08-17 22:25:29 +0300

    Made changes to the code

--- create_csv.py
+++ create_csv.py
@@ -9,24 +9,18 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         
-        print(f"Processing file: {xml_file}")  # Print the current XML file being processed
-        
         filename = root.find('filename').text
-        print(f"  Filename: {filename}")  # Print the filename
         
         width = int(root.find('size').find('width').text)
         height = int(root.find('size').find('height').text)
-        print(f"  Width: {width}, Height: {height}")  # Print the width and height
         
         for member in root.findall('object'):
             class_name = member.find('name').text
-            print(f"  Class Name: {class_name}")  # Print the class name
             
             xmin = int(member.find('bndbox').find('xmin').text)
             ymin = int(member.find('bndbox').find('ymin').text)
             xmax = int(member.find('bndbox').find('xmax').text)
             ymax = int(member.find('bndbox').find('ymax').text)
-            print(f"  Bounding Box: {xmin}, {ymin}, {xmax}, {ymax}")  # Print the bounding box
             
             xml_list.append((filename, width, height, class_name, xmin, ymin, xmax, ymax))
             
@@ -34,5 +28,3 @@ def xml_to_csv(path):
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     
     return xml_df
-
-
